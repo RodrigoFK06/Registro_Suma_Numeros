e8b7aba6bcc3cc1eef0d5e4e7e8f00ba72c348c6
jorgeanthony torrespastor <torrespastor4@hotmail.com>
2023-10-02 20:34:25 -0500

    Se agregó el metodo Registrar y Mostrar

---
+++ Caso2_Registro_Suma_Numeros/Metodo mostrar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caso2_Registro_Suma_Numeros
+{
+    internal class Metodo_mostrar
+    {
+       /* private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            string connectionString = "server=TONY; database=bdSumaNumeros; integrated security=true";
+            string query = "SELECT CAST(num1 AS decimal(18, 2)) AS num1, CAST(num2 AS decimal(18, 2)) AS num2, CAST(suma AS decimal(18, 2)) AS suma FROM numeros";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                System.Data.DataTable dataTable = new System.Data.DataTable();
+                adapter.Fill(dataTable);
+
+                dataListado.DataSource = dataTable; // Asigna los datos al DataGridView
+            }
+        }*/
+    }
+}
